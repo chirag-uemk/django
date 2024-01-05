139a0ce1f2705926b28824c1a7eb70d58e3d9444
chirag-uemk <140333955+chirag-uemk@users.noreply.github.com>
2024-01-05 06:09:15 +0530

    Add files via upload

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact 
+from django.contrib import messages
 def index(request):
     #return HttpResponse("Babletali")
-    context={
-        "variable":" Djemba Djemba"
-    }
-    return render(request,'index.html',context)
+    return render(request,'index.html')
 def about(request):
-    return HttpResponse(" About Babletali")
+    #return HttpResponse(" About Babletali")
+    return render(request,'about.html')
 def services(request):
-    return HttpResponse(" Service Babletali")
+    #return HttpResponse(" Service Babletali")
+    return render(request,'services.html')
 def contact(request):
-    return HttpResponse(" Contact Babletali")
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+    return render(request,'contact.html')
+    #return HttpResponse(" Contact Babletali")
 
 # Create your views here.
